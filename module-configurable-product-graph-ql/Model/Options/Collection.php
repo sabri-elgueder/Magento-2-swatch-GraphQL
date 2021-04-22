3d3076c8c2dc2af6671a5c836be24abd3497b42b
Sabri <sabri.elgueder@satoripop.com>
2021-04-22 10:20:26 +0100

    Fix Bug patch

--- module-configurable-product-graph-ql/Model/Options/Collection.php
+++ module-configurable-product-graph-ql/Model/Options/Collection.php
@@ -49,6 +49,14 @@ class Collection
     private $productIds = [];
 
     /**
+     * We need it in order to add product model to the 'values' and use it for getting swatch data
+     * @see Magento\SwatchesGraphQl\Model\Resolver\Product\Options\DataProvider\SwatchDataProvider
+     *
+     * @var Product[]
+     */
+    private $productModels = [];
+
+    /**
      * @var array
      */
     private $attributeMap = [];
@@ -75,6 +83,53 @@ class Collection
             ->get(Uid::class);
     }
 
+     /**
+     * Add product model.
+     *
+     * @param Product $productModel
+     *
+     * @return void
+     */
+    public function addProductModel(Product $productModel) : void
+    {
+        $productId = $productModel->getId();
+        if (!array_key_exists($productId, $this->productModels)) {
+            $this->productModels[$productId] = $productModel;
+        }
+    }
+
+    /**
+     * Get product model by product id.
+     *
+     * @param string $productId
+     *
+     * @return Product|null
+     */
+    private function getProductModel(string $productId) : ?Product
+    {
+        return array_key_exists($productId, $this->productModels) ? $this->productModels[$productId] : null;
+    }
+
+    /**
+     * Add product model to each value item.
+     *
+     * @param array $array
+     * @param string $productId
+     *
+     * @return array
+     */
+    private function addProductModelToValues(array $array, string $productId) : array
+    {
+        foreach ($array as &$value) {
+            if (!is_array($value)) {
+                continue;
+            }
+
+            $value['model'] = $this->getProductModel($productId);
+        }
+
+        return $array;
+    }
     /**
      * Add product id to attribute collection filter.
      *
@@ -146,13 +201,16 @@ class Collection
                 $this->uidEncoder->encode((string) $attribute->getProductId());
             $this->attributeMap[$productId][$attribute->getId()]['attribute_code'] =
                 $attribute->getProductAttribute()->getAttributeCode();
-            $this->attributeMap[$productId][$attribute->getId()]['values'] = array_map(
-                function ($value) use ($attribute) {
-                    $value['attribute_id'] = $attribute->getAttributeId();
-                    return $value;
-                },
-                $attributeData['options']
-            );
+                            // we need to add product model to the values for getting swatch data in the SwatchData resolver
+            $values = $this->addProductModelToValues($attributeData['options'], (string)$productId);
+            $this->attributeMap[$productId][$attribute->getId()]['values'] = $values;
+            // $this->attributeMap[$productId][$attribute->getId()]['values'] = array_map(
+            //     function ($value) use ($attribute) {
+            //         $value['attribute_id'] = $attribute->getAttributeId();
+            //         return $value;
+            //     },
+            //     $attributeData['options']
+            // );
             $this->attributeMap[$productId][$attribute->getId()]['label']
                 = $attribute->getProductAttribute()->getStoreLabel();
         }
